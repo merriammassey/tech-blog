4edc6a30c6038089ae864474274276eb53ecad44
Maria M <prov21_1@hotmail.com>
2021-05-14 13:00:03 -0700

    jaws code

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
 // import the Sequelize constructor from the library
 const Sequelize = require("sequelize");
 
-//import the base Sequelize class and create a new connection to the database
-const sequelize = new Sequelize(
-  "tech_blog_db",
-  "root",
-  "mysqlPassword1",
-  //process.env.DB_NAME,
-  //process.env.DB_USER,
-  //process.env.DB_PW,
-  {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  }
-);
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize("tech_blog_db", "root", "mysqlPassword1", {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    });
 
 module.exports = sequelize;
