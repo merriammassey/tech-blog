8f2f49235c622fc1ad60304695441f671a62d917
Maria M <prov21_1@hotmail.com>
2021-05-14 18:40:33 -0700

    logout

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -99,4 +99,14 @@ router.get("/:id", (req, res) => {
     });
 });
 
+//log out route
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 module.exports = router;
