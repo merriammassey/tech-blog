df5614aa9874db3ee873d2a928f90853f2cadce1
Maria M <prov21_1@hotmail.com>
2021-05-14 19:52:19 -0700

    comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,21 +3,18 @@ const { Comment } = require("../../models");
 
 router.post("/", (req, res) => {
   // check the session
-  //if (req.session) {
-  Comment.create({
-    comment_text: req.body.comment_text,
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-
-    // use the id from the session
-    //user_id: req.session.user_id,
-  })
-    .then((dbCommentData) => res.json(dbCommentData))
-    .catch((err) => {
+  if (req.session) {
+    Comment.create({
+      comment_text: req.body.comment_text,
+      post_id: req.body.post_id,
+      // use the id from the session
+      user_id: req.session.user_id,
+    }).catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
-  //}
+  }
+  res.redirect("/post/" + req.body.post_id);
 });
 
 router.get("/", (req, res) => {
